d39eff03627a4f397f45a8227a63140336b3b9de
Linas Jonika <linas.jonika1@gmail.com>
2021-01-27 00:50:53 +0200

    Still need to fix the O instead of X

--- js/main.js
+++ js/main.js
@@ -1,1 +1,67 @@
-ECHO is on.
+/*----- constants -----*/
+const winningCombos = [
+[0, 1, 2],
+[3, 4, 5],
+[6, 7, 8],
+[0, 3, 6], 
+[1, 4, 7],
+[2, 5, 8],
+[0, 4, 8],
+[2, 4, 6]
+];
+
+
+/*----- app's state (variables) -----*/
+
+let board;
+let turn = 'X';
+let win;
+/*----- cached element references -----*/
+const squares = Array.from(document.querySelectorAll('#board div'));
+const messages = document.querySelector('h2');
+
+/*----- event listeners -----*/
+
+document.getElementById('board').addEventListener('click', handleTurn);
+//new code
+document.getElementById('reset-button').addEventListener('click', init);
+/*----- functions -----*/
+
+function getWinner() {
+let winner = null;
+winningCombos.forEach((combo, index) => {
+if (board[combo[0]] && board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]]) {
+winner = board[combo[0]];
+}
+});
+// new code below
+return winner ? winner : board.includes('') ? null : 'T';
+};
+function init(){
+board = [
+'', '', '',
+'', '', '',
+'', '', ''
+];
+
+render()
+};
+
+init();
+
+function render() {
+	board.forEach(function(mark, index){
+	console.log(mark, index);
+	squares[index].textContent = mark;
+	});
+	messages.textContent = win === 'T' ? `That's a tie, queen!` : win ? `${win} wins the game!` : `It's ${turn}'s turn!`;
+}
+function handleTurn() {
+let idx = squares.findIndex(function(square) {
+return square === event.target;
+});
+board[idx] = turn;
+// new code here
+win = getWinner();
+render();
+};
